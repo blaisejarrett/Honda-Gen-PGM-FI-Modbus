56f4f9078e943125f0adc7db4e184fc8a8235efe
Blaise Jarrett <blaisejarrett@gmail.com>
2024-06-25 23:00:52 -0700

    Setting up a class structure in advanced for query caching.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,44 @@
 #include <Arduino.h>
 #include <ModbusIP_ESP8266.h>
 #include <WiFiManager.h>
+#include <pgmfi_decoder.h>
 
 ModbusIP mb;
 WiFiManager wm;
 
-const int TEST_HREG = 100;
+struct PGM_Base {
+  unsigned long ecu1_last_q_time;
+  const uint16_t mb_addr = 0;
+  const uint16_t len = 0;
+  uint16_t * values; 
+
+  PGM_Base(uint16_t mb_addr_, uint16_t len_): 
+    mb_addr(mb_addr_), len(len_) {
+      this->values = (uint16_t *)malloc(sizeof(uint16_t) * this->len);
+  }
+
+  virtual uint16_t getValue(uint16_t address);
+
+  uint16_t getValue(TRegister* reg) {
+    uint16_t addr = reg->address.address - this->mb_addr;
+    return this->getValue(addr);
+  };
+};
+
+// A place to store all the data from the generator.
+struct MB_ECU_Info1 : DLC::ECU_Info1, PGM_Base {
+  MB_ECU_Info1() : PGM_Base(100, 8) {}
+  
+  using PGM_Base::getValue;
+  virtual uint16_t getValue(uint16_t address) {
+    return 40 + address;
+  };
+} ecu1;
+
+// DLC::ECU_Info2 ecu2;       // 6
+// DLC::Inverter_Master inv1; // 5
+// DLC::Inverter_Slave inv2;  // 3
+
 
 
 void setup() {
@@ -17,7 +50,12 @@ void setup() {
     Serial.println("Failed to connect.");
 
   mb.server();
-  mb.addHreg(TEST_HREG, 0xABCD);
+
+  // set up ECU info1
+  mb.addIreg(ecu1.mb_addr, 0, ecu1.len);
+  mb.onGetIreg(ecu1.mb_addr, [](TRegister* reg, uint16_t) {
+    return ecu1.getValue(reg);
+  }, ecu1.len);
 }
 
 
@@ -25,4 +63,3 @@ void loop() {
   // put your main code here, to run repeatedly:
   mb.task();
 }
-
